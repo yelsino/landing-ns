a26231cd930ab03076f6e878e66cba3a0e739b8f
yelsino <yelsino321@gmail.com>
2022-05-01 17:46:24 -0500

    zero3

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import pluginRewriteAll from 'vite-plugin-rewrite-all';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    pluginRewriteAll()
-  ]
+  plugins: [react()]
 })
